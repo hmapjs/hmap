4f0d88038b36b9e848efce4bd6d29e56a5548d49
moonrailgun <moonrailgun@email.com>
2017-04-21 09:20:30 +0800

    test

---
+++ test/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var hmap = require('../index');
+var fs = require('fs');
+
+var html = fs.readFileSync(__dirname + '/cases/github-full.html', 'utf8');
+console.time("sample");
+console.log(hmap(__dirname + "/cases/github.hmap",  html));
+console.timeEnd("sample");
---
+++ test/map.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var fs = require('fs');
+var lex = require('hmap-lexer');
+var parse = require('hmap-parser');
+var mapAst = require('../lib/map-ast');
+
+var filename = 'github.hmap';
+var testCase = fs.readFileSync(__dirname + '/cases/' + filename, 'utf8');
+var tokens = lex(testCase, {filename});
+var ast = parse(Object.assign([], tokens), {filename, src : testCase});
+
+// console.log(JSON.stringify(ast));
+console.log(mapAst(ast,{filename}));
