684c832daec827b2b173f593941bee1d69f881c0
moonrailgun <moonrailgun@gmail.com>
2017-09-21 09:16:55 +0800

    ..

--- test/index.js
+++ test/index.js
@@ -7,3 +7,10 @@ var html = fs.readFileSync(__dirname + '/cases/github-full.html', 'utf8');
 console.time("sample");
 console.log(hmap(__dirname + "/cases/github.hmap", html));
 console.timeEnd("sample");
+
+
+// 2222
+var html = fs.readFileSync(__dirname + '/cases/test2.html', 'utf8');
+console.time("sample2");
+console.log(hmap(__dirname + "/cases/test2.hmap", html));
+console.timeEnd("sample2");
